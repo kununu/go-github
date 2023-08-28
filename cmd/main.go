9aaf4f91e1c2dc5d4080993036bf9e472e0904e8
André Sá <enkodr@outlook.com>
2023-08-28 08:59:37 +0100

    Add PR creation functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 
 	"github.com/kununu/go-github"
 )
 
 var (
-	appId  string
-	instId string
+	appId  int64
+	instId int64
 	key    string
 )
 
 func init() {
-	flag.StringVar(&appId, "a", "", "App ID to use for authentication")
 	flag.StringVar(&key, "k", "", "Path to key file for authentication")
-	flag.StringVar(&instId, "i", "", "Installation ID that identifies the APP installation ID on GitHub")
+	flag.Int64Var(&appId, "a", 0, "App ID to use for authentication")
+	flag.Int64Var(&instId, "i", 0, "Installation ID that identifies the APP installation ID on GitHub")
 	flag.Parse()
 }
 
 func main() {
-	keyBytes := []byte{}
 	// Get the values from the environment variables if they are set with parameters
-	if appId == "" {
-		appId = os.Getenv("GITHUB_APP_ID")
+	if appId == 0 {
+		appIdInt, _ := strconv.ParseInt(os.Getenv("GITHUB_APP_ID"), 10, 64)
+		appId = appIdInt
 	}
-	if instId == "" {
-		instId = os.Getenv("GITHUB_INST_ID")
+	if instId == 0 {
+		instIdInt, _ := strconv.ParseInt(os.Getenv("GITHUB_INST_ID"), 10, 64)
+		instId = instIdInt
 	}
 	if key == "" {
 		key = os.Getenv("GITHUB_KEY_PATH")
@@ -45,46 +45,23 @@ func main() {
 				fmt.Printf("you need to pass the private key either with `-k` parameter or by setting GITHUB_KEY_PATH or even passing through STDIN\n")
 				os.Exit(1)
 			}
-			var lines []string
-			reader := bufio.NewReader(os.Stdin)
-			for {
-				// read line from stdin using newline as separator
-				line, _ := reader.ReadString('\n')
-				// if line is empty, break the loop
-				if len(strings.TrimSpace(line)) == 0 {
-					break
-				}
-				//append the line to a slice
-				lines = append(lines, line)
-				keyBytes = append(keyBytes, []byte(line)...)
-			}
 			key = "stdin"
 		}
 	}
 
 	// Verify if the necessary information is set
-	if appId == "" || instId == "" {
+	if appId == 0 || instId == 0 {
 		fmt.Println("You need to define the App ID and the path to the key file")
 		fmt.Println("by passing the values the -a, -i and -k options or")
 		fmt.Println("by setting GITHUB_APP_ID, GITHUB_INST_ID and GITHUB_KEY_PATH environment variables.")
-		os.Exit(0)
-	}
-
-	// Read the key from the file
-	if key != "stdin" {
-		var err error
-		keyBytes, err = os.ReadFile(key)
-		if err != nil {
-			fmt.Println("error reading the key file")
-			os.Exit(1)
-		}
+		os.Exit(1)
 	}
 
 	// Create a new GitHubApp
 	ghApp, err := github.NewGitHubApp(&github.GitHubAppConfig{
 		ApplicationID:  appId,
 		InstallationID: instId,
-		PrivateKey:     keyBytes,
+		PrivateKey:     key,
 	})
 	if err != nil {
 		fmt.Println(err.Error())
